55ef572d6a6d39a3f2e43e9a1e2c584084c24895
Heba-Webdev23 <jtvonline@yahoo.com>
2023-08-03 21:37:38 +0300

    Added Why component to the main route

--- app/page.js
+++ app/page.js
@@ -1,8 +1,11 @@
 import Hero from "@/components/home/Hero";
+import Why from "@/components/home/Why";
+
 export default function Home() {
   return (
     <main className="h-screen bg-orange-50">
       <Hero />
+      <Why />
     </main>
   );
 }
