69c4c834ca72a3a074afb30e8030ad8e3e0b8be2
Heba-Webdev23 <jtvonline@yahoo.com>
2023-08-03 21:36:28 +0300

    Fixed svg classes from sperated by hyphen to camelCase

--- components/home/Hero.js
+++ components/home/Hero.js
@@ -5,13 +5,13 @@ export default function Hero() {
   return (
     <div className="relative h-full w-full bg-hero bg-cover bg-center bg-no-repeat">
       <div className="">
-        <h1 className="pt-[8rem] text-center text-5xl font-extrabold text-white md:pt-[6rem] md:text-8xl">
+        <h1 className="pt-[8rem] text-center text-5xl font-extrabold italic text-white md:pt-[6rem] md:text-8xl">
           Dream vacation starts here!
         </h1>
       </div>
 
       <Link
-        href="#"
+        href="/"
         className="absolute bottom-8 left-[43%] mt-3 flex flex-col justify-center self-center text-center text-sm uppercase text-white md:left-[47.3%] "
       >
         Learn more
@@ -20,13 +20,13 @@ export default function Hero() {
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
         viewBox="0 0 24 24"
-        stroke-width="1.5"
+        strokeWidth="1.5"
         stroke="currentColor"
-        class="absolute bottom-1 left-[50%] h-6 w-6 self-center text-center text-sm text-white"
+        className="absolute bottom-1 left-[50%] h-6 w-6 self-center text-center text-sm text-white"
       >
         <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeLinecap="round"
+          strokeLinejoin="round"
           d="M19.5 8.25l-7.5 7.5-7.5-7.5"
         />
       </svg>
