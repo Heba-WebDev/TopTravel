f83b5d637e695d1f21e06508d636c15e427796c4
Heba-Webdev23 <jtvonline@yahoo.com>
2023-08-04 21:08:02 +0300

    Added the ExploreDestinations componant

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import Hero from "@/components/home/Hero";
 import Why from "@/components/home/Why";
+import ExploreDestinations from "@/components/home/ExploreDestinations";
 
 export default function Home() {
   return (
     <main className="h-screen bg-orange-50">
       <Hero />
       <Why />
+      <ExploreDestinations />
     </main>
   );
 }
