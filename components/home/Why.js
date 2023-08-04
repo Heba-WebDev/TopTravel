a3ac6492bd3eae5c869557b8de8598bf3a2d33b7
Heba-Webdev23 <jtvonline@yahoo.com>
2023-08-04 21:10:02 +0300

    Changed the copywrite

--- components/home/Why.js
+++ components/home/Why.js
@@ -3,18 +3,19 @@ import Link from "next/link";
 export default function Why() {
   return (
     <div className="bg-orange-50 py-[8rem]">
-      <div className="container mx-auto grid w-[100%] gap-8 text-center md:w-[70%]">
+      <div className="container-custom mx-auto grid w-[100%] gap-8 text-left md:w-[70%] md:text-center">
         <h2 className="text-4xl font-extrabold italic md:text-6xl">
-          Next-level travel powered by tech and the human touch
+          Revolutionary journeys fueled by innovation and personal care
         </h2>
         <p className="mx-auto w-[100%] md:w-[85%]">
-          Our travel advisors have the tools, insights & connections to build
-          your dream vacation. Get killer recommendations & perks, a seamless
-          booking experience — and maybe a cool new friend along the way.
+          Our trip planners possess the expertise, resources, and networks to
+          craft your ideal holiday. Receive exceptional suggestions and
+          benefits, a smooth reservation process, and perhaps even a new
+          companion along the way.
         </p>
         <Link
           href="/"
-          className="mx-auto w-[70%] bg-black px-2 py-3 uppercase text-white md:w-[30%]"
+          className=" w-[40%] border border-black bg-black px-2 py-3 text-center uppercase text-white hover:bg-orange-50 hover:text-black hover:transition hover:duration-300 hover:ease-in-out md:mx-auto md:w-[30%]"
         >
           Why Top Travel
         </Link>
